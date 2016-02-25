9f0ebd660d62eeb2d3e48a1d1fdc05a3b1c611c4
KiraShubik <kira.shubik@gmail.com>
2016-02-25 15:03:23 +0300

    Add function for rename tasks

--- builds/dev/app/home/home.js
+++ builds/dev/app/home/home.js
@@ -17,33 +17,42 @@
    * Home Factory
    */
   // @ngInject
-  function tasksFactory($q, $http, dbc, $firebaseArray, $firebaseObject, registration, $rootScope){
+  function tasksFactory($q, $http, dbc, $firebaseArray, $firebaseObject, registration, $rootScope) {
     var o = {};
 
-     var ref = dbc.getRef();
-     var authRef = dbc.isLogin();
-     var curUserRef = authRef.uid;
-     var usersRef = ref.child('users');
-     var userRef = usersRef.child(curUserRef);
-     var userTasks = userRef.child('tasks');
+    var ref = dbc.getRef();
+    var authRef = dbc.isLogin();
+    var curUserRef = authRef.uid;
+    var usersRef = ref.child('users');
+    var userRef = usersRef.child(curUserRef);
+    var userTasks = userRef.child('tasks');
 
 
-     var tasks = [];
 
-    o.getAllTasks = function(){
+    var tasks = [];
+
+    o.getAllTasks = function() {
       tasks = $firebaseArray(userTasks);
       return tasks;
     }
-    o.addNewTask = function(_newTask){
+    o.addNewTask = function(_newTask) {
       tasks.$add({
         name: _newTask.name || "(Без названия)",
         time: _newTask.time || "(Не определено)",
         cost: _newTask.cost || "(Не определена)"
       })
     }
-    o.deletTask = function(_task){
+    o.deletTask = function(_task) {
       var taskID = _task.$id;
       return $firebaseObject(userTasks.child(taskID)).$remove();
+    }
+    o.saveNewName = function(_editTask, _newName) {
+      var THISTASK = _editTask.$id
+      var obj = $firebaseObject(userTasks.child(THISTASK))
+      obj.name = _newName;
+      obj.time = _editTask.time;
+      obj.cost = _editTask.cost;
+      obj.$save().then(function() {});
     }
     return o;
   }
@@ -66,6 +75,9 @@
       time: null,
       cost: null
     };
+    s.showEditingInput = false;
+    s.newTaskName = null;
+    s.editTASK = null;
 
     var startDate, curDate, curTimerStart, timerVal, timer = null;
 
@@ -78,12 +90,12 @@
         timerVal = timerVal + curDate;
         s.startBtnText = 'Продолжить';
       } else {
-        if(!startDate){
+        if (!startDate) {
           startDate = new Date();
           timerVal = 0;
         }
         curTimerStart = new Date();
-        timer = $interval(function(){
+        timer = $interval(function() {
           curDate = new Date() - curTimerStart;
           s.timer = timerVal + curDate;
         }, 1000);
@@ -92,7 +104,7 @@
     };
 
     s.reset = function() {
-      if(timer){
+      if (timer) {
         $interval.cancel(timer);
         timer = null;
       }
@@ -107,6 +119,20 @@
     s.deletTask = function(_d) {
       tasks.deletTask(_d)
     }
+    s.editTask = function(_task) {
+      s.showEditingInput = true;
+      s.editTASK = _task;
+      console.log("editTaskFunction", s.editTASK)
+
+    }
+    s.doneEditing = function() {
+      s.showEditingInput = false;
+      console.log("doneEditingFunction", s.newTaskName)
+      tasks.saveNewName(s.editTASK, s.newTaskName);
+      s.newTaskName = null;
+      s.editTASK = null;
+    }
+
 
     $rootScope.currentPage = 'home';
   };
